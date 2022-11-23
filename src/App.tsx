058d3a93298310568276e5f1cf7dc58d01ecfc3f
OmarDaSilva <72036965+OmarDaSilva@users.noreply.github.com>
2022-11-23 18:27:32 +0000

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import "@ionic/react/css/core.css";
 
 function App() {
+
+  const makeAPICall = async () => {
+    try {
+      const response = await fetch('http://localhost:5001/getInvoices', {mode:'cors'});
+      const data = await response.json();
+      console.log({ data })
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    makeAPICall();
+  }, [])
+  // const { error, isLoaded, items } = this.state;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div className="">
+
+   </div>
+  )
 }
 
 export default App;
